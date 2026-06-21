d259b8ff7175064ea7355dc22806a85c1108199f
Middhas <joaquim.tavares.costa.8@gmail.com>
2026-06-21 14:23:54 +0100

    Adiciona blocos de gameplay e ajustes de paleta

--- js/main.js
+++ js/main.js
@@ -2,10 +2,8 @@
 // Ponto de entrada do JavaScript.
 // Importa e inicializa todos os módulos.
 
-import { initTheme } from "./theme.js";
 import { initNavbar } from "./navbar.js";
 import { initTerminal } from "./terminal.js";
 
-initTheme();
 initNavbar();
 initTerminal();
